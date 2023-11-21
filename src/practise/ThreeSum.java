4b4b088d0efa83f6c984a91185c0d1efb5ef1f9e
G.M.Nazmul Hossain (Somrat) <somratcste@gmail.com>
2023-11-21 10:01:18 +0100

    Create ThreeSum.java

---
+++ src/practise/ThreeSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+// O(n2) | O(n)
+class Program {
+  public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+    Arrays.sort(array);
+    List<Integer[]> triplets = new ArrayList<Integer[]>();
+    for (int i = 0; i < array.length - 2; i++) {
+      int left = i+1;
+      int right = array.length-1;
+      while (left < right) {
+        int currentSum = array[i] + array[left] + array[right];
+        if (currentSum == targetSum) {
+          Integer[] newTriplet = {array[i], array[left], array[right]};
+          triplets.add(newTriplet);
+          left++;
+          right--;
+        } else if (currentSum < targetSum) {
+          left++;
+        } else if (currentSum > targetSum) {
+          right--;
+        }
+      }
+    }
+    return triplets;
+  }
+}
